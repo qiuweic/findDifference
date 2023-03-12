840ac1567bf6521efcaee026f95c20710c9cae97
qiuweic <qiuweic@gmail.com>
2023-03-12 23:52:22 +0800

    update codes

--- app/src/main/java/com/liete/game/fd/huawei/MainActivity.java
+++ app/src/main/java/com/liete/game/fd/huawei/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         levelId = getCurrentLevelId();
-        levelId = 80;
         currentLevel.setText("Level " + levelId);
     }
 
